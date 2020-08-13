0dc475a18350003e2dced5c4b6d57edc1ceeb1de
Luís Filipe Velasco da Silva <velasco@pop-os.localdomain>
2020-08-13 15:48:02 -0300

    Starting the use of enviroment variables

--- src/database/__main__.py
+++ src/database/__main__.py
@@ -1,19 +1,25 @@
+import os
+import sys
 import argparse
 
 from sqlalchemy import create_engine
 
 from .scheme import Base
+from .app import main
+
+
+DATABASE_ENGINE = os.getenv('DATABASE_ENGINE')
 
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Declaration of the database scheme of the aplication')
-	parser.add_argument('--database', default='sqlite:///sqlalchemy_example.db',
+	parser.add_argument('--database', default=DATABASE_ENGINE,
 						type=str, help="Which database the script should run on.")
 	
 	group = parser.add_mutually_exclusive_group()
 	group.add_argument('--create_scheme', action='store_true', help="Generates the scheme inside the database.")
 	group.add_argument('--delete_scheme', action='store_true', help="Deletes the scheme inside the database.")
-
+	group.add_argument('--start', action='store_true', help="Starts the rest application")
 #	insert = parser.add_subparser("insert")#database insert parser
 
 	args = parser.parse_args()
@@ -26,5 +32,7 @@ if __name__ == '__main__':
 		print("scheme created")
 	elif args.delete_scheme:
 		raise NotImplemented("Deletion of scheme not implemented.")
+	elif args.start:
+		main()
 	else:
 		parser.print_help()
