7887fba8dfef99c6c4272cd8f3b67b1ff549c28c
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2020-08-28 17:42:03 -0300

    Selection of services to be run via argparse available

--- src/__main__.py
+++ src/__main__.py
@@ -1,26 +1,39 @@
 import argparse
+import sys
 
 from . import app
+from .apis import api, config_api
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Execution of full application')
 
-	parser.add_argument('--database', default=None, type=str, help="Database url for connection.")
-
 
 	group = parser.add_mutually_exclusive_group()
-	group.add_argument('-r', '--run', action='store_true', help="Runs the REST application")
+	group.add_argument('-r', '--run', action='store_true', help="Runs the full REST application")
 	group.add_argument('-t', '--test', action='store_true', help='Runs the unittest')
 
 	parser.add_argument('-d', '--debug', action='store_true', help="Runs with debug")
 	parser.add_argument('-p', '--port', default=5000, help="Which port to run the application on")
+	parser.add_argument('-s', '--services', nargs='+', help='specifies the REST services to be runned. It is case sensitive!')
+	parser.add_argument('--database', default=None, type=str, help="Database url for connection.")
 
 	args = parser.parse_args()
 
-	if args.database is not None:
-		app.config['DATABASE_URL'] = args.database
-	else:
-		app.config['DATABASE_URL'] = f'http://localhost:{args.port}'
+	#If some services are selected,
+	#either database is one of those services
+	#or the url to access the database must be provided
+	if not args.services is None:
+		if 'database' in args.services and args.database is None:
+			app.config['DATABASE_URL'] = f'http://localhost:{args.port}'
+		elif not ('database' in args.services or args.database is None):
+			#database is parsed
+			app.config['DATABASE_URL'] = args.database
+		else:
+			print("case 3")
+			print("The database must be one of the services or it's url must be parsed via --database.")
+			sys.exit()
+
+	config_api(args.services)
 
 	if args.run:
 		app.run(debug=args.debug, port=args.port)
--- src/apis/__init__.py
+++ src/apis/__init__.py
@@ -1,13 +1,32 @@
 from flask_restx import Api
 
-from .database import app as app1
-from .user import app as app2
-
 api = Api(
 	title='Qfila'
 )
 
-api.add_namespace(app1.ns)
-api.add_namespace(app2.ns)
-
 app = None
+
+#create function here for the imports
+#it's easier to not add the namespace than to delete
+#intention of this 'not adding' is for the capability
+#of selecting which services to run.
+def config_api(libs):
+	import importlib
+
+	if libs is None:
+		libs = ('database', 'user')
+	try:
+		for service_name in libs:
+			service = importlib.import_module('.app', f"{__package__}.{service_name}")
+
+			api.add_namespace(service.ns)
+			service.ns._path = service_name
+	except ModuleNotFoundError as e:
+		import sys
+
+		module = e.msg.split('.')[-2]
+		if module == 'apis':
+			module = e.msg.split('.')[-1][:-1]
+
+		print("There is no service named '{}'".format(module))
+		sys.exit()
