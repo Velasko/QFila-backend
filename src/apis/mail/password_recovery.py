2cbb660cf4ff641c982c05751d7e2222df467a99
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2020-09-17 22:51:22 -0300

    Mocking up email sending to avoid spamming 	modified:   mail/password_recovery.py

--- src/apis/mail/password_recovery.py
+++ src/apis/mail/password_recovery.py
@@ -12,6 +12,8 @@ class PasswordRecovery(Resource):
 		msg = Message("Qfila esqueci minha senha", recipients=[email])
 		msg.html = f'Você requisitou uma mudança de senha. <a href="{link}">Clique aqui</a>'
 
-		mail.send(msg)
+		print("E-mail wasn't sent for considering this a mockup for test and development.\n" + \
+			"However, this is the message:\n" + msg.html)
+		# mail.send(msg)
 
 		return {'message' : 'email sent'}
