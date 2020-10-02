498d47cad9414f8d6591c62a3f327bbdfeaea228
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2020-10-01 23:17:07 -0300

    user service standalone

--- src/apis/user/__main__.py
+++ src/apis/user/__main__.py
@@ -2,37 +2,38 @@ import os
 
 from flask import Flask
 
-from .app import api
+from .app import blueprint
+from .config import Config
 
 
 if __name__ == '__main__':
 	import argparse
 
 	app = Flask("Qfila user")
-
-	#adding the configurations from app
-	exec(''.join(open("{}/back-end/src/app_config.py".format(os.getenv('VIRTUAL_ENV')), 'r').readlines()))
-	config(app)
-
-	api.init_app(app)
+	config = Config(app)
 
 	parser = argparse.ArgumentParser(description='User interface section of the back-end')
-
-	parser.add_argument('--database', required=True, type=str, help="Database url for connection.")
 
 	group = parser.add_mutually_exclusive_group()
 	group.add_argument('-r', '--run', action='store_true', help="Runs the REST application")
 	group.add_argument('-t', '--test', action='store_true', help='Runs the unittest')
 
 	parser.add_argument('-d', '--debug', action='store_true', help="Runs with debug")
+	parser.add_argument('--host', default='127.0.0.1', help="defines the host")
 	parser.add_argument('-p', '--port', default=5000, help="Which port to run the application on")
+
+	for service in ("database", "mail"):
+		parser.add_argument(f'--{service}', required=True, type=str, help=f"{service} url for connection.")
 
 	args = parser.parse_args()
 
 	app.config['DATABASE_URL'] = args.database
+	app.config['MAIL_URL'] = args.mail
 
 	if args.run:
-		app.run(debug=args.debug, port=args.port)
+		app.register_blueprint(blueprint, url_prefix="/")
+		config.configure()
+		app.run(host=args.host, debug=args.debug, port=args.port)
 	elif args.test:
 		raise NotImplementedError("Tests for User interface not implemented")
 	else:
