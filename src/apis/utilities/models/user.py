ccb7b9a10f28344afcba3f34d90f98509ebab2d0
Velasco <f.l.velasko@gmail.com>
2021-03-28 23:26:49 -0300

    bug fixes and minor improvment

--- src/apis/utilities/models/user.py
+++ src/apis/utilities/models/user.py
@@ -47,7 +47,7 @@ history_response = order_contents.inherit("history_response", {
 	"payment_method" : fields.String(enum=payment_methods),
 })
 
-user_update = Model("user.update"{
+user_update = Model("user.update", {
 	"old_password" : fields.String(require=True),
 	"user" : fields.Nested(user)
 })
